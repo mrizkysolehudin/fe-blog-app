b4170990c6ec9c9e5d0fed567ce81fdd8a489397
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2022-12-07 21:35:35 +0700

    finished fitur login (pages/users/login)

--- src/pages/Users/Login/Login.js
+++ src/pages/Users/Login/Login.js
@@ -5,7 +5,37 @@ import {
 	TrendingUpIcon,
 } from "@heroicons/react/outline";
 
+import * as Yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
+import { useFormik } from "formik";
+import { loginUserAction } from "redux/slices/users/usersSlices";
+import { Link, Navigate } from "react-router-dom";
+
+const formSchema = Yup.object({
+	email: Yup.string().required("Email is required"),
+	password: Yup.string().required("Password is required"),
+});
+
 export default function Login() {
+	const dispatch = useDispatch();
+
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+		},
+		onSubmit: (values) => {
+			dispatch(loginUserAction(values));
+		},
+		validationSchema: formSchema,
+	});
+
+	// redirect
+	const store = useSelector((state) => state?.users);
+	const { userAuth, loading, serverErr, appErr } = store;
+
+	if (userAuth) return <Navigate to={`/profile/${userAuth?._id}`} />;
+
 	return (
 		<div className="bg-gray-900 min-h-screen">
 			<div className=" max-w-5xl mx-auto flex pt-16">
@@ -14,37 +44,70 @@ export default function Login() {
 						Login to your Account
 					</h3>
 
-					<form>
-						<div className="mb-12">
+					<form onSubmit={formik.handleSubmit}>
+						{serverErr || appErr ? (
+							<p className="text-red-500">
+								{serverErr} - {appErr}
+							</p>
+						) : (
+							""
+						)}
+						<div className="mb-8">
 							<input
-								type="text"
-								name="email"
+								value={formik.values.email}
+								onChange={formik.handleChange("email")}
+								onBlur={formik.handleBlur("email")}
+								type="email"
 								placeholder="enter email"
-								className="w-full placeholder:font-bold placeholder-gray-300 pl-20 pr-4"
+								className="w-full placeholder:font-bold placeholder-gray-300 pl-20 py-2"
 							/>
-							<LockOpenIcon className="w-5 h-6 relative -mt-6 ml-6 text-gray-500" />
+							<LockOpenIcon className="w-6 h-6 relative -mt-8 ml-6 text-gray-500" />
+							{/* Err message */}
+							<p className="text-red-500 mt-2 ml-6">
+								{formik.touched.email && formik.errors.email}
+							</p>
 						</div>
 
 						<div>
 							<input
-								type="text"
-								name="email"
+								value={formik.values.password}
+								onChange={formik.handleChange("password")}
+								onBlur={formik.handleBlur("password")}
+								type="password"
 								placeholder="Password"
-								className="w-full placeholder:font-bold placeholder-gray-300 pl-20 pr-4"
+								className="w-full placeholder:font-bold placeholder-gray-300 pl-20 py-2"
 							/>
-							<LockClosedIcon className="w-5 h-6 relative -mt-6 ml-6 text-gray-500" />
+							<LockClosedIcon className="w-6 h-6 relative -mt-8 ml-6 text-gray-500" />
+							{/* err message */}
+							<p className="text-red-500 mt-2 ml-6">
+								{formik.touched.password &&
+									formik.errors.password}
+							</p>
 						</div>
 
 						<div>
-							<button className="bg-blue-500 w-full rounded-full py-4 font-bold text-white mt-10 hover:bg-blue-600">
-								Login
-							</button>
-
-							<button className="text-blue-800 hover:text-blue-600 font-semibold pt-2 pl-1">
-								Forgot Password?
-							</button>
+							{loading ? (
+								<button
+									disabled
+									className="bg-blue-500 w-full rounded-full py-4 font-bold text-white mt-10 hover:bg-blue-600">
+									Loading...
+								</button>
+							) : (
+								<button
+									type="submit"
+									className="bg-blue-500 w-full rounded-full py-4 font-bold text-white mt-10 hover:bg-blue-600">
+									Login
+								</button>
+							)}
 						</div>
 					</form>
+					<div className="pt-2">
+						<Link
+							to="/password-reset-token"
+							className="text-blue-800 hover:text-blue-600 font-semibold pl-1">
+							Forgot Password?
+						</Link>
+					</div>
 				</section>
 				<section className="w-7/12 text-center mt-24 pl-16">
 					<div className="w-20 h-20 justify-center flex items-center rounded-lg bg-blue-500 mx-auto mb-12">
